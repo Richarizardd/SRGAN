b3b8be413851165f72a473617dcf1f63c39a30a5
Richard Chen <richardchen@RichardChen-MBP.local>
2018-08-26 02:08:01 -0700

    Changing Evaluate.py

--- Evaluate.py
+++ Evaluate.py
@@ -1,11 +1,14 @@
-import math
-import os
+import math, os, re, sys
 
 import numpy as np
+from PIL import Image
 import skimage
 from skimage import measure
-from PIL import Image
 
+def natural_sort(l): 
+    convert = lambda text: int(text) if text.isdigit() else text.lower() 
+    alphanum_key = lambda key: [ convert(c) for c in re.split('([0-9]+)', key) ] 
+    return sorted(l, key = alphanum_key)
 
 def psnr(img1, img2):
     mse = np.mean( (img1 - img2) ** 2 )
@@ -14,67 +17,36 @@ def psnr(img1, img2):
     PIXEL_MAX = 255.0
     return 20 * math.log10(PIXEL_MAX / math.sqrt(mse))
 
+def evaluate(path):
+    PSNRs_resize, PSNRs_pred = [], []
+    SSIMs_resize, SSIMs_pred = [], []
 
+    test_dir = os.listdir(path)
+    test_dir.sort()
 
+    for i in np.arange(0, len(test_dir), 3):
+        if i % 60 == 0:
+            print i, i+1, i+2
 
-SSIMs_pred_dense, PSNRs_pred_dense = [], []
-SSIMs_resize_dense, PSNRs_resize_dense = [], []
+        gt = np.array(Image.open(path+test_dir[i]))
+        pred = np.array(Image.open(path+test_dir[i+1]))
+        resize = np.array(Image.open(path+test_dir[i+2]))
 
-path = "results/4_Dense_Test/"
-test_dir = os.listdir(path)
-test_dir.sort()
+        SSIMs_pred.append(measure.compare_ssim(gt, pred, multichannel=True))
+        SSIMs_resize.append(measure.compare_ssim(gt, resize, multichannel=True))
 
-print "Evaluating:", path
-for i in np.arange(0, len(test_dir), 3):
-    if i % 60 == 0:
-        print i, i+1, i+2
+        PSNRs_pred.append(psnr(gt, pred))
+        PSNRs_resize.append(psnr(gt, resize))
 
-    gt = np.array(Image.open(path+test_dir[i]))
-    pred = np.array(Image.open(path+test_dir[i+1]))
-    resize = np.array(Image.open(path+test_dir[i+2]))
+    print "SSIM Pred:", np.mean(SSIMs_pred)
+    print "SSIM Resize:", np.mean(SSIMs_resize)
+
+    print "PSNR Pred:", np.mean(PSNRs_pred)
+    print "PSNR Resize:", np.mean(PSNRs_resize)
     
-    SSIMs_pred_dense.append(measure.compare_ssim(gt, pred, multichannel=True))
-    SSIMs_resize_dense.append(measure.compare_ssim(gt, resize, multichannel=True))
-    
-    PSNRs_pred_dense.append(psnr(gt, pred))
-    PSNRs_resize_dense.append(psnr(gt, resize))
+def main()
+    path = sys.argv[1]
+    evaluate(path)
 
-print "SSIM Pred:", np.mean(SSIMs_pred_dense)
-print "SSIM Resize:", np.mean(SSIMs_resize_dense)
-
-print "PSNR Pred:", np.mean(PSNRs_pred_dense)
-print "PSNR Resize:", np.mean(PSNRs_resize_dense)
-
-
-print 
-
-SSIMs_pred_res, PSNRs_pred_res = [], []
-SSIMs_resize_res, PSNRs_resize_res = [], []
-
-path = "results/4_Residual_Test/"
-test_dir = os.listdir(path)
-test_dir.sort()
-
-print "Evaluating:", path
-for i in np.arange(0, len(test_dir), 3):
-    if i % 60 == 0:
-        print i, i+1, i+2
-        
-    name1, name2, name3 = test_dir[i].split("_")[0], test_dir[i+1].split("_")[0], test_dir[i+2].split("_")[0]
-    assert name1 == name2 == name3
-    
-    gt = np.array(Image.open(path+test_dir[i]))
-    pred = np.array(Image.open(path+test_dir[i+1]))
-    resize = np.array(Image.open(path+test_dir[i+2]))
-    
-    SSIMs_pred_res.append(measure.compare_ssim(gt, pred, multichannel=True))
-    SSIMs_resize_res.append(measure.compare_ssim(gt, resize, multichannel=True))
-    
-    PSNRs_pred_res.append(psnr(gt, pred))
-    PSNRs_resize_res.append(psnr(gt, resize))
-
-print "SSIM Pred:", np.mean(SSIMs_pred_res)
-print "SSIM Resize:", np.mean(SSIMs_resize_res)
-
-print "PSNR Pred:", np.mean(PSNRs_pred_res)
-print "PSNR Resize:", np.mean(PSNRs_resize_res)
+if __name__ == "__main__":
+    main()
